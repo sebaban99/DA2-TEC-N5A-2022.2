0ca94f30295586cae6680f1b8a6d7cf5c9c219e0
Sebastián Bañales <sebaban99@gmail.com>
2022-10-20 00:45:25 -0300

    Angular initial project

---
+++ VidlyWebApp/VidlyWebApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MoviesComponent } from './movies/movies.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
